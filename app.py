43c1c91d969f54c2e9375e638f1dec6fbad59eec
Andrey <Dr_Wo1f@mail.ru>
2025-06-08 22:23:42 +0300

    add models.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
+from models import db
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+db.init_app(app)
 
 
-@app.route('/index/')
+@app.route('/')
 def index():
     return render_template("base.html")
 
