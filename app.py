9f4ba064ebf67791d703217f4ea689be2b6e783f
Andrey <Dr_Wo1f@mail.ru>
2025-06-11 15:25:55 +0300

    add some fix

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from models import db
-
+from models import db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
@@ -11,6 +10,13 @@ db.init_app(app)
 @app.route('/')
 def index():
     return render_template("base.html")
+
+
+@app.cli.command("init-db")
+def init_db():
+    db.create_all()
+    print("OK")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -4,11 +4,28 @@ from datetime import datetime
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class Parents(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(40), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    kids = db.Column()
 
     def __repr__(self):
         return f'User({self.username}, {self.email})'
+
+
+class Students(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+
+
+class Teachers(db.Model):
+    # учителя, фио, предмет, классное руководство
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class Subjects(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name_of_sub = db.Column(db.String(30), unique=True, nullable=False)
