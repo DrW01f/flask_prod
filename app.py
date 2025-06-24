e77dff564171c78eeceb5a61e1745f5019a4fcb0
Andrey <Dr_Wo1f@mail.ru>
2025-06-24 20:56:38 +0300

    fix auto-fill

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from models import db, User
+from models import db, Parents, Students, Teachers
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
@@ -16,6 +16,24 @@ def index():
 def init_db():
     db.create_all()
     print("OK")
+
+
+@app.cli.command("auto-fill")
+def auto_filling_data_in_db():
+    user = Students(age=9, class_number=3, student_name="Иванов Андрей Алексеевич")
+    db.session.add(user)
+    db.session.commit()
+    print("Auto filling OK")
+
+
+@app.cli.command("edit-student")
+def edit_student():
+    # name = input()
+    student = Students.query.filter_by(student_name="Иванов Андрей Алексеевич").first()
+    # student.age += 1
+    student.class_number = 1
+    db.session.commit()
+    print("Edit complete")
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -4,28 +4,35 @@ from datetime import datetime
 db = SQLAlchemy()
 
 
+class Students(db.Model):
+    # главная таблица
+    student_id = db.Column(db.Integer, primary_key=True)
+    student_name = db.Column(db.String(30), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    class_number = db.Column(db.Integer, nullable=False)
+    parents = db.relationship("Parents", backref="kid", lazy=True)
+
+    def __repr__(self):
+        return f'User({self.username}, {self.age}, {self.class_number})'
+
+
 class Parents(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    parent_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
     email = db.Column(db.String(40), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    kids = db.Column()
+    kids = db.Column(db.Integer, db.ForeignKey("students.student_id"))
 
     def __repr__(self):
-        return f'User({self.username}, {self.email})'
-
-
-class Students(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), unique=True, nullable=False)
-    age = db.Column(db.Integer, nullable=False)
+        return f'User({self.username}, {self.kids})'
 
 
 class Teachers(db.Model):
     # учителя, фио, предмет, классное руководство
-    id = db.Column(db.Integer, primary_key=True)
+    teacher_id = db.Column(db.Integer, primary_key=True)
+    teacher_name = db.Column(db.String(30), nullable=False)
 
 
 class Subjects(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    subject_id = db.Column(db.Integer, primary_key=True)
     name_of_sub = db.Column(db.String(30), unique=True, nullable=False)
