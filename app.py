f4584253bfb1f1ae73036ca6c8c772e4a0c6eff0
Andrey <Dr_Wo1f@mail.ru>
2025-06-26 20:51:59 +0300

    return

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ def init_db():
 
 @app.cli.command("auto-fill")
 def auto_filling_data_in_db():
+    # расширить функционал для заполнения всех таблиц 5 записями
     user = Students(age=9, class_number=3, student_name="Иванов Андрей Алексеевич")
     db.session.add(user)
     db.session.commit()
@@ -30,11 +31,44 @@ def auto_filling_data_in_db():
 def edit_student():
     # name = input()
     student = Students.query.filter_by(student_name="Иванов Андрей Алексеевич").first()
-    # student.age += 1
+    student.age += 1
     student.class_number = 1
     db.session.commit()
     print("Edit complete")
 
 
+@app.cli.command("del-student")
+def del_student():
+    student = Students.query.filter_by(student_name="Иванов Андрей Алексеевич").first()
+    # лучше не удалять, а сделать статус "невидимый" или "удаленный"
+    db.session.delete(student)
+    db.session.commit()
+    print("Delete complete")
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# 42:00
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
